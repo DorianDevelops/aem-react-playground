619671d560a302e6bd775cb7560b6129f3169faa
ddominguez <ddominguez1@caesars.com>
2018-10-24 16:31:54 -0700

    Started tut playground created dorian component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Page, withModel, EditorContext, Utils } from '@adobe/cq-react-editable-components';
-import { Redirect } from 'react-router';
-import Header from './components/header/Header';
-
+import React from "react";
+import { Page, withModel, EditorContext, Utils } from "@adobe/cq-react-editable-components";
+import { Redirect } from "react-router";
+import Header from "./components/header/Header";
+import DorianComponent from "./components/dorian-component/DorianComponent";
 
 /**
  * Returns a model path from the given URL
@@ -10,12 +10,12 @@ import Header from './components/header/Header';
  * @return {string|undefined}
  */
 function getModelPath(url) {
-    if (!url) {
-        return;
-    }
+  if (!url) {
+    return;
+  }
 
-    let dotIndex = url.indexOf(".");
-    return url.substr(0, dotIndex > -1 ? dotIndex : url.length);
+  let dotIndex = url.indexOf(".");
+  return url.substr(0, dotIndex > -1 ? dotIndex : url.length);
 }
 
 /**
@@ -25,42 +25,42 @@ function getModelPath(url) {
  * @return {boolean}
  */
 function canRedirectHome(modelUrl, pathname) {
-    if (!pathname) {
-        return false;
-    }
-    const currentUrl = getModelPath(pathname);
-    // 1. if a model url has been provided
-    // 2. if the current URL is located under the content
-    // 3. if app root model path equals the current URL
-    return modelUrl && modelUrl.indexOf('/content/') > -1 && (modelUrl === currentUrl || modelUrl.endsWith(currentUrl));
+  if (!pathname) {
+    return false;
+  }
+  const currentUrl = getModelPath(pathname);
+  // 1. if a model url has been provided
+  // 2. if the current URL is located under the content
+  // 3. if app root model path equals the current URL
+  return modelUrl && modelUrl.indexOf("/content/") > -1 && (modelUrl === currentUrl || modelUrl.endsWith(currentUrl));
 }
 
 // This component is the application entry point
 class App extends Page {
+  get redirect() {
+    const modelRootPath = this.props.cqPath;
+    const locationPathname = this.props.locationPathname;
 
-    get redirect() {
-        const modelRootPath = this.props.cqPath;
-        const locationPathname = this.props.locationPathname;
-
-        if (canRedirectHome(modelRootPath, locationPathname)) {
-            // Redirect to the home url
-            return <Redirect to={ modelRootPath + '/home.html' }/>;
-        }
-        return null;
+    if (canRedirectHome(modelRootPath, locationPathname)) {
+      // Redirect to the home url
+      return <Redirect to={modelRootPath + "/home.html"} />;
     }
+    return null;
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <Header navigationRoot="/content/wknd-events/react/home" />
-                <EditorContext.Provider value={ Utils.isInEditor() }>
-                    { this.redirect }
-                    { this.childComponents }
-                    { this.childPages }
-                </EditorContext.Provider>
-            </div>
-          );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Header navigationRoot="/content/wknd-events/react/home" />
+        <DorianComponent test="dorian dawg"/>
+        <EditorContext.Provider value={Utils.isInEditor()}>
+          {this.redirect}
+          {this.childComponents}
+          {this.childPages}
+        </EditorContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default withModel(App);
